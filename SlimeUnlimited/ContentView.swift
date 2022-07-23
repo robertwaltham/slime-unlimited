375cfc88150124cae72b7ef719706d87ce41c4bc
Robert Waltham <rob@blockoftext.com>
2022-07-23 16:30:57 -0700

    Initial Commit

---
+++ SlimeUnlimited/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SlimeUnlimited
+//
+//  Created by Robert Waltham on 2022-07-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SlimeUnlimited/SlimeUnlimitedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlimeUnlimitedApp.swift
+//  SlimeUnlimited
+//
+//  Created by Robert Waltham on 2022-07-23.
+//
+
+import SwiftUI
+
+@main
+struct SlimeUnlimitedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
